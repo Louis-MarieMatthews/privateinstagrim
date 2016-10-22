0f12356ef28bbc8035b84868490481dfd0e1f9fb
Louis-Marie Matthews <internet657-github@yahoo.fr>
2016-10-22 21:48:48 +0100

    Update code style notice

--- src/main/java/uk/ac/dundee/computing/aec/instagrim/models/PicModel.java
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/models/PicModel.java
@@ -5,10 +5,13 @@
  * Some rules are still not applied yet.
  * However, some rules won't be followed:
  * 1. No underscore suffix at the end of private variables (r8)
- * 2. No space between a function and its parenthesis (r74)
- * 3. Class and package names (can't be changed, lead to problems) (r3)
+ * 2. No space between a function and its parenthesis (r74). Instead, parenthesis
+ * may be wrapped around space. So function ( parameter ) instead of 
+ * function (parameter).
  * 4. Abbreviations and the use of init is okay (r17, r24)
  * 5. Statements and variable declarations don't need to be aligned (r77, r78)
+ * 6. Class names don't have to be nouns (would make some class' names long and
+ * poorly representative for servlets and filters).
  */
 package uk.ac.dundee.computing.aec.instagrim.models;
 
