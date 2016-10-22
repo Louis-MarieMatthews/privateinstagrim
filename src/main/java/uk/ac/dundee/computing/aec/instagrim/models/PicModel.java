a65885479cb30a3ce358186970563f8a1a51a2ec
Louis-Marie Matthews <internet657-github@yahoo.fr>
2016-10-22 20:23:58 +0100

    Fix typo in keyspaces initialisation

--- src/main/java/uk/ac/dundee/computing/aec/instagrim/models/PicModel.java
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/models/PicModel.java
@@ -79,7 +79,7 @@ public class PicModel
       
       CassandraHosts.query( "INSERT INTO pics ("
         + "picid, image, thumb, processed, user, interaction_time, imagelength,"
-        + "thumblength, procesedlength, type, name )"
+        + "thumblength, processedlength, type, name )"
         + "VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
         picid, buffer, thumbbuf, processedbuf, user, DateAdded, length, thumblength, processedlength, type, name );
       CassandraHosts.query( "INSERT INTO userpiclist ( picid, user, pic_added ) values ( ?, ?, ? )", picid, user, DateAdded );
